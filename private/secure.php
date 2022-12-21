7502b7cac88060c5d1ddc781b0756546f3eb2d01
Raymond Brian D. Hernandez <35060465+raymondbrianhernandez@users.noreply.github.com>
2022-12-21 10:22:07 -0800

    Delete private directory

--- private/secure.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /* Paste on all pages that needs to be logged in first */
-   session_start();
-   if(empty($_SESSION['logged_in'])) {
-      header('Location: index.php');
-      exit;
-   }
-   /* End of secure script */
-?>
