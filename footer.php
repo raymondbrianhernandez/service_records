df24e9bd38c7f151747a1d426d85b6a2599b6816
Raymond Brian D. Hernandez <35060465+raymondbrianhernandez@users.noreply.github.com>
2022-12-21 10:41:58 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+ 
+    <footer id="footer">
+    &copy; <?php echo date('Y'); ?> Service Records | <a href="https://www.arbhie.com" target="_blank">arbhie.com</a> by Raymond Hernandez
+    </footer>  
+  </body>
+</html>
+
+<?php db_disconnect($con); ?>
