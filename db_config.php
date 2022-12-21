50835938a8d8d075074e1c23691a809b6140304c
Raymond Brian D. Hernandez <35060465+raymondbrianhernandez@users.noreply.github.com>
2022-12-21 10:41:38 -0800

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -12,10 +12,10 @@
 
    // LIVE SERVER
    /*
-   $host        = "127.0.0.1:3306";  
-   $user        = "u803325201_root";  
-   $password    = "P@ssword1";  
-   $db_name     = "u803325201_service_record";
+   $host        = "";  
+   $user        = "";  
+   $password    = "";  
+   $db_name     = "";
    $con         = mysqli_connect($host, $user, $password, $db_name);
    
    if(mysqli_connect_errno()) {  
